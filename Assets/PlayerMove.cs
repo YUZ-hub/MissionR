0b6b12ec3d5fc207adc496e1e5a06072d5623cab
YUZ-hub <jackyyeh66@gmail.com>
2022-08-12 10:34:16 +0800

    Add pixel art
    add pistol character art
    remove old pistol and missile

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -6,6 +6,7 @@ public class PlayerMove : MonoBehaviour
 {
     [SerializeField] private float speed, rotateSpeed;
     [SerializeField] private Camera cam;
+    [SerializeField] private Transform gunHoldTransform;
 
     private Vector2 destination, mousePos;
     private Rigidbody2D rb;
@@ -37,11 +38,24 @@ public class PlayerMove : MonoBehaviour
     private void Rotate()
     {
         Vector2 direction = (mousePos - (Vector2)transform.position).normalized;
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotateSpeed);
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;    
+        if ((direction.x * transform.localScale.x) < 0)
+        {
+            Flip();
+        }
+        if( direction.x<0)
+        {
+            angle += 180;
+        }
+        gunHoldTransform.rotation = Quaternion.Slerp(gunHoldTransform.rotation, Quaternion.AngleAxis(angle, Vector3.forward), rotateSpeed);
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.AngleAxis(angle, Vector3.forward), rotateSpeed);
     }
-
+    private void Flip()
+    {
+        Vector3 temp = transform.localScale;
+        temp.x *= -1;
+        transform.localScale = temp;
+    }
     private void Move()
     {
         Vector2 direction = (destination - (Vector2)transform.position).normalized;
